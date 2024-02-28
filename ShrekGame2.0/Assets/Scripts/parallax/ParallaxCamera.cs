bc21194b9f9e295b8fd9c057edad4b427587980c
kirill667 <gubickijkirill918@gmail.com>
2024-02-28 16:46:56 +0700

    1

--- ShrekGame2.0/Assets/Scripts/parallax/ParallaxCamera.cs
+++ ShrekGame2.0/Assets/Scripts/parallax/ParallaxCamera.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class ParallaxCamera : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public delegate void ParallaxCameraDelegate(float deltaMovement);
+    public ParallaxCameraDelegate onCameraTranslate;
+
+    private float oldPosition;
+
     void Start()
     {
-        
+        oldPosition = transform.position.x;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position.x != oldPosition)
+        {
+            if (onCameraTranslate != null)
+            {
+                float delta = oldPosition - transform.position.x;
+                onCameraTranslate(delta);
+            }
+
+            oldPosition = transform.position.x;
+        }
     }
 }
--- ShrekGame2.0/Assets/Scripts/parallax/parallaxlayer.cs
+++ ShrekGame2.0/Assets/Scripts/parallax/parallaxlayer.cs
@@ -2,17 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class ParallaxLayer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float parallaxFactor;
+
+    public void Move(float delta)
     {
-        
+        Vector3 newPos = transform.localPosition;
+        newPos.x -= delta * parallaxFactor;
+
+        transform.localPosition = newPos;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
